5b81bc0188d00a2157f338608fc22712188bc3d1
gss2002 <greg@senia.org>
2018-11-01 11:32:57 -0400

    Update mqClientTest and gitignore

--- src/main/java/mqclient/MQClientTest.java
+++ src/main/java/mqclient/MQClientTest.java
@@ -36,8 +36,7 @@ public class MQClientTest {
 		String line;
 		try {
 			while ((line = bufferedReader.readLine()) != null) {
-				stringBuffer.append(line);
-				stringBuffer.append("\n");
+				testSendMessageQueue(qmgrName, queue, line);
 			}
 			fileReader.close();
 			message = stringBuffer.toString();
@@ -45,56 +44,54 @@ public class MQClientTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		testSendMessageQueue(qmgrName, queue, message);
 
-		
+
 	}
 
-    public static void testSendMessageQueue(String qmgrName, String queueName, String mqMessage) {
-        // Create a connection to the queue manage
+	public static void testSendMessageQueue(String qmgrName, String queueName, String mqMessage) {
+		// Create a connection to the queue manage
 
-        MQQueueManager qMgr = null;
-        try {
-            qMgr = new MQQueueManager(qmgrName);
+		MQQueueManager qMgr = null;
+		try {
+			qMgr = new MQQueueManager(qmgrName);
 
-            // MQOO_OUTPUT = Open the queue to put messages. The queue is opened for use with subsequent MQPUT calls.
-            // MQOO_INPUT_AS_Q_DEF = Open the queue to get messages using the queue-defined default.
-            // The queue is opened for use with subsequent MQGET calls. The type of access is either
-            // shared or exclusive, depending on the value of the DefInputOpenOption queue attribute.
-            //int openOptions = MQConstants.MQOO_OUTPUT | MQConstants.MQOO_INPUT_AS_Q_DEF;
-            int openOptions = MQConstants.MQOO_OUTPUT;
-            // creating destination
-            MQQueue queue = qMgr.accessQueue(queueName, openOptions);
+			// MQOO_OUTPUT = Open the queue to put messages. The queue is opened for use with subsequent MQPUT calls.
+			// MQOO_INPUT_AS_Q_DEF = Open the queue to get messages using the queue-defined default.
+			// The queue is opened for use with subsequent MQGET calls. The type of access is either
+			// shared or exclusive, depending on the value of the DefInputOpenOption queue attribute.
+			//int openOptions = MQConstants.MQOO_OUTPUT | MQConstants.MQOO_INPUT_AS_Q_DEF;
+			int openOptions = MQConstants.MQOO_OUTPUT;
+			// creating destination
+			MQQueue queue = qMgr.accessQueue(queueName, openOptions);
 
-            // specify the message options...
-            MQPutMessageOptions pmo = new MQPutMessageOptions(); // default
-            // MQPMO_ASYNC_RESPONSE = The MQPMO_ASYNC_RESPONSE option requests that an MQPUT or MQPUT1 operation
-            // is completed without the application waiting for the queue manager to complete the call.
-            // Using this option can improve messaging performance, particularly for applications using client bindings.
-        		pmo.options = pmo.options + MQConstants.MQPMO_NO_SYNCPOINT;
+			// specify the message options...
+			MQPutMessageOptions pmo = new MQPutMessageOptions(); // default
+			// MQPMO_ASYNC_RESPONSE = The MQPMO_ASYNC_RESPONSE option requests that an MQPUT or MQPUT1 operation
+			// is completed without the application waiting for the queue manager to complete the call.
+			// Using this option can improve messaging performance, particularly for applications using client bindings.
+			pmo.options = pmo.options + MQConstants.MQPMO_NO_SYNCPOINT;
 
-            // create message
-            MQMessage message = new MQMessage();
-            // MQFMT_STRING = The application message data can be either an SBCS string (single-byte character set),
-            // or a DBCS string (double-byte character set). Messages of this format can be converted
-            // if the MQGMO_CONVERT option is specified on the MQGET call.
-            message.format = MQConstants.MQFMT_STRING;
-            message.writeString(mqMessage);
-            queue.put(message, pmo);
-            queue.close();
+			// create message
+			MQMessage message = new MQMessage();
+			// MQFMT_STRING = The application message data can be either an SBCS string (single-byte character set),
+			// or a DBCS string (double-byte character set). Messages of this format can be converted
+			// if the MQGMO_CONVERT option is specified on the MQGET call.
+			message.format = MQConstants.MQFMT_STRING;
+			message.writeString(mqMessage);
+			queue.put(message, pmo);
+			queue.close();
 
-            MQAsyncStatus asyncStatus = qMgr.getAsyncStatus();
-        } catch (MQException e) {
-        		e.printStackTrace();
-        } catch (IOException e) {
-    			e.printStackTrace();
-        } finally {
-            try {
-                qMgr.disconnect();
-            } catch (MQException e) {
-        			e.printStackTrace();
-            }
-        }
-    }
+			MQAsyncStatus asyncStatus = qMgr.getAsyncStatus();
+		} catch (MQException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				qMgr.disconnect();
+			} catch (MQException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
-
